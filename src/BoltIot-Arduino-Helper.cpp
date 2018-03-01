9f536514a6392779cf728273a2a59159a43ffb86
vinayakjoshi <vinayakjoshi@192.168.0.3>
2018-03-01 12:58:08 +0530

    Changed function and variable names to match Arduino naming style

--- src/BoltIot-Arduino-Helper.cpp
+++ src/BoltIot-Arduino-Helper.cpp
@@ -2,55 +2,55 @@
 #include <Arduino.h>
 #include "BoltIot-Arduino-Helper.h"
 
-BoltIot boltiot;
+BoltIoT boltiot;
 
-BoltIot::BoltIot() {
+BoltIoT::BoltIoT() {
     this->commandList = NULL;
     this->argumentReadTimeout = -1;
 }
 
-void BoltIot::Begin(HardwareSerial& uart) {
-    this->communication_port = &uart;
+void BoltIoT::begin(HardwareSerial& uart) {
+    this->communicationPort = &uart;
     uart.begin(9600);
 }
 
-void BoltIot::Begin(int Rx_Pin, int Tx_Pin) {
-    this->communication_port = new SoftwareSerial(Rx_Pin, Tx_Pin);
-    ((SoftwareSerial*)this->communication_port)->begin(9600);
-    ((SoftwareSerial*)this->communication_port)->listen();
+void BoltIoT::begin(int Rx_Pin, int Tx_Pin) {
+    this->communicationPort = new SoftwareSerial(Rx_Pin, Tx_Pin);
+    ((SoftwareSerial*)this->communicationPort)->begin(9600);
+    ((SoftwareSerial*)this->communicationPort)->listen();
 }
 
-void BoltIot::CheckPoll(float a, float b, float c, float d, float e, float f) {
+void BoltIoT::CheckPoll(float a, float b, float c, float d, float e, float f) {
     int length;
-    if (this->communication_port->available() > 0) {
-        char data = this->communication_port->read();
-        this->command_received += data;
-        if (this->command_received.equals("RD\r")) {
-            this->communication_port->print(a);
-            this->communication_port->print(",");
-            this->communication_port->print(b);
-            this->communication_port->print(",");
-            this->communication_port->print(c);
-            this->communication_port->print(",");
-            this->communication_port->print(d);
-            this->communication_port->print(",");
-            this->communication_port->print(e);
-            this->communication_port->print(",");
-            this->communication_port->println(f);
-            this->command_received = "";
+    if (this->communicationPort->available() > 0) {
+        char data = this->communicationPort->read();
+        this->receivedString += data;
+        if (this->receivedString.equals("RD\r")) {
+            this->communicationPort->print(a);
+            this->communicationPort->print(",");
+            this->communicationPort->print(b);
+            this->communicationPort->print(",");
+            this->communicationPort->print(c);
+            this->communicationPort->print(",");
+            this->communicationPort->print(d);
+            this->communicationPort->print(",");
+            this->communicationPort->print(e);
+            this->communicationPort->print(",");
+            this->communicationPort->println(f);
+            this->receivedString = "";
         }
-        length = this->command_received.length();
+        length = this->receivedString.length();
         if (length > 2) {
-            this->command_received = this->command_received.substring(length - 2, length);
+            this->receivedString = this->receivedString.substring(length - 2, length);
         }
     }
 }
 
-String BoltIot::GetCommandString() {
-    return this->command_received;
+String BoltIoT::getReceivedString() {
+    return this->receivedString;
 }
 
-void BoltIot::SetCommandString(String command, String(*command_function)(String*), int number_of_arguments, char dataDelimiter) {
+void BoltIoT::setCommandString(String command, String(*command_function)(String*), int number_of_arguments, char dataDelimiter) {
     /*
      * This function is called to parse possible functions to be received from the Bolt.
      * The command string defines the possible function.
@@ -63,74 +63,74 @@ void BoltIot::SetCommandString(String command, String(*command_function)(String*
     if (commandList == NULL) {
         commandList = new CommandList();
         temp = commandList;
-        temp->next = NULL;
+        temp->nextCommand = NULL;
     } else {
-        while ((temp->next != NULL)&&(!temp->command.equals(command))) {
-            temp = temp->next;
+        while ((temp->nextCommand != NULL)&&(!temp->command.equals(command))) {
+            temp = temp->nextCommand;
         }
         if (!temp->command.equals(command)) {
-            temp->next = new CommandList();
-            temp = temp->next;
-            temp->next = NULL;
+            temp->nextCommand = new CommandList();
+            temp = temp->nextCommand;
+            temp->nextCommand = NULL;
         }
     }
     temp->command = command;
-    temp->command_function = command_function;
-    temp->number_of_arguments = number_of_arguments;
+    temp->commandCallback = command_function;
+    temp->numberOfArguments = number_of_arguments;
     temp->dataDelimiter = dataDelimiter;
 }
 
-void BoltIot::HandleCommand() {
+void BoltIoT::handleCommand() {
     /*
      * This function replaces checkpoll. and should be called periodically.
      */
     CommandList *temp;
 
-    while (this->communication_port->available() > 0) {
-        char data = this->communication_port->read();
+    while (this->communicationPort->available() > 0) {
+        char data = this->communicationPort->read();
         if (this->currentMonitoredCommand != NULL) {
             bool runCommandSuccessful = this->runCommand(*(this->currentMonitoredCommand), data);
             if (runCommandSuccessful) {
-                this->communication_port->println(this->currentMonitoredCommand->returnData);
+                this->communicationPort->println(this->currentMonitoredCommand->returnData);
                 this->currentMonitoredCommand = NULL;
             }
             continue;
         }
-        this->command_received += data;
+        this->receivedString += data;
         temp = this->commandList;
         while ((temp != NULL)&&(this->currentMonitoredCommand == NULL)) {
-            if (checkIfCommandReceived(temp->command)) {
+            if (isCommandReceived(temp->command)) {
                 this->currentMonitoredCommand = temp;
-                if (this->currentMonitoredCommand->number_of_arguments > 0) {
-                    this->currentMonitoredCommand->arguments = new String[this->currentMonitoredCommand->number_of_arguments];
+                if (this->currentMonitoredCommand->numberOfArguments > 0) {
+                    this->currentMonitoredCommand->arguments = new String[this->currentMonitoredCommand->numberOfArguments];
                 }
                 this->currentMonitoredCommand->numberOfArgumentsFound = -2;
                 bool runCommandSuccessful = this->runCommand(*(this->currentMonitoredCommand), this->currentMonitoredCommand->dataDelimiter);
                 if (runCommandSuccessful) {
-                    this->communication_port->println(this->currentMonitoredCommand->returnData);
+                    this->communicationPort->println(this->currentMonitoredCommand->returnData);
                     this->currentMonitoredCommand = NULL;
                 }
-                this->command_received = "";
+                this->receivedString = "";
                 break;
             }
-            temp = temp->next;
+            temp = temp->nextCommand;
         }
     }
 }
 
-bool BoltIot::checkIfCommandReceived(String command) {
-    int length = this->command_received.length() - command.length();
+bool BoltIoT::isCommandReceived(String command) {
+    int length = this->receivedString.length() - command.length();
     if (length < 0) {
         return false;
     }
-    if (command.equals(this->command_received.substring(length))) {
+    if (command.equals(this->receivedString.substring(length))) {
         return true;
     }
     return false;
 }
 
-bool BoltIot::runCommand(CommandList &commandStruct, char data) {
-    if (commandStruct.number_of_arguments > 0) {
+bool BoltIoT::runCommand(CommandList &commandStruct, char data) {
+    if (commandStruct.numberOfArguments > 0) {
         if (commandStruct.numberOfArgumentsFound == -2) {
             commandStruct.numberOfArgumentsFound = -1;
             return false;
@@ -142,7 +142,7 @@ bool BoltIot::runCommand(CommandList &commandStruct, char data) {
         }
         if (data == commandStruct.dataDelimiter) {
             commandStruct.numberOfArgumentsFound++;
-            if (commandStruct.numberOfArgumentsFound != commandStruct.number_of_arguments) {
+            if (commandStruct.numberOfArgumentsFound != commandStruct.numberOfArguments) {
                 return false;
             }
         } else {
@@ -150,7 +150,7 @@ bool BoltIot::runCommand(CommandList &commandStruct, char data) {
             return false;
         }
     }
-    String returnValue = commandStruct.command_function(commandStruct.arguments);
+    String returnValue = commandStruct.commandCallback(commandStruct.arguments);
     if (returnValue.substring((returnValue.length() - 1)).equals("\n")) {
         returnValue = returnValue.substring(0, returnValue.length() - 1);
     }
--- src/BoltIot-Arduino-Helper.h
+++ src/BoltIot-Arduino-Helper.h
@@ -16,37 +16,45 @@
 /* This structure holds all the commands set by the user.
  */
 
-struct CommandList{
-    String command;
-    String (*command_function)(String *);
-    int number_of_arguments;
-    int numberOfArgumentsFound;
-    String* arguments;
-    CommandList *next;
-    String returnData;
-    char dataDelimiter;
-};
-
 /* Class which handles the communication with bolt over UART
  * The UART can be any hardware serial or software serial port
  */
-class BoltIot{
-    Stream *communication_port; //Holds the communication port at which Bolt is connected.
-    String command_received;    //Holds the command currently being passed
+class BoltIoT {
+
+    class CommandList {
+    public:
+        String command; //Holds the command to be checked for.
+        String(*commandCallback)(String *); // Hold the callback related to the command.
+        int numberOfArguments; // Number of arguments that will be sent along with the command.
+        int numberOfArgumentsFound; //Number of arguments currently found.
+        String* arguments; // String containing the arguments.
+        String returnData; //Holds the data returned by the callback, which has to be sent back
+        char dataDelimiter; //the character which will separate the command from arguments. Example SetPin;pinNumber=3;pinState=HIGH;
+        CommandList *nextCommand; // This forms a linked list, linking to the next command in the list.
+    }; //This class is defined here, to ensure that it only works with the Bolt IoT Arduino Helper library 
+
+    Stream *communicationPort; //Holds the communication port at which Bolt is connected.
+    String receivedString; //Holds the command currently being passed
+
+    CommandList *commandList;
+    bool isCommandReceived(String command);
+    bool runCommand(CommandList &commandStruct, char data);
+    long argumentReadTimeout;
+    CommandList *currentMonitoredCommand;
 public:
-    
-    BoltIot();
+
+    BoltIoT();
     /* Used to initialize the bolt class with Hardware Serial port. The function starts the uart at 9600, the required baud rate for bolt
      * uart-Hardware serial port to which Bolt is connected. Can be Serial, Serial0, Serial1
      */
-    void Begin(HardwareSerial& uart);
+    void begin(HardwareSerial& uart);
 
 
     /* Used to initialize the bolt class with Software Serial port. The function starts the software uart at 9600, the required baud rate for bolt
      * Rx_Pin- The arduino pin number to which the Tx pin of Bolt is connected.
      * Tx_Pin- The arduino pin number to which the Rx pin of Bolt is connected.
      */
-    void Begin(int Rx_Pin,int Tx_Pin);
+    void begin(int Rx_Pin, int Tx_Pin);
 
 
     /* This function has to be called from the loop function.
@@ -54,35 +62,29 @@ public:
      * If the arduino has received the read command from the bolt, the functions sends back 6 float csv's, which are provided as the variables
      * At the least 1 variable has to be set by the user.
      */
-    void CheckPoll(float a,float b=0,float c=0,float d=0,float e=0,float f=0);
-    
-    
+    void CheckPoll(float a, float b = 0, float c = 0, float d = 0, float e = 0, float f = 0);
+
+
     /* 
      * @Future_development
      * This function iterates through the whole list of commands vs command functions and calls the command function depending on the command received.
      */
-    
-    void HandleCommand();
-    
+
+    void handleCommand();
+
     /* @Future_Development:
      * This string currently only provides the data buffered in the bolt class.
      */
-    String GetCommandString();
-    
+    String getReceivedString();
+
     /* @Future_Development:
      * This 
      */
-    void SetCommandString(String command, String (*command_function)(String *),int number_of_arguments=0,char dataDelimiter=' ');
-    
+    void setCommandString(String command, String(*command_function)(String *), int number_of_arguments = 0, char dataDelimiter = ' ');
+
     void SetArgumentTimeout(long timeout);
-private:
-    CommandList *commandList;
-    bool checkIfCommandReceived(String command);
-    bool runCommand(CommandList &commandStruct,char data);
-    long argumentReadTimeout;
-    CommandList *currentMonitoredCommand;
 };
 
-extern BoltIot boltiot; //Variable defined in the header so that user does not have to define the variable
+extern BoltIoT boltiot; //Variable defined in the header so that user does not have to define the variable
 #endif /* BOLT_H */
 
